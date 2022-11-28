d89beea45cfdb928439b2c2cddf010c1f9faff5e
Spintly <nayakshrameet33@gmail.com>
2022-11-28 21:05:14 +0530

    updated code

--- update database/updateToDatabase.py
+++ update database/updateToDatabase.py
@@ -9,28 +9,68 @@ connection = psycopg2.connect(database="diagnostics_prod",
 connection.autocommit = True
 cursor = connection.cursor()
 
-data = pd.read_csv("org.csv")
+gatedata = pd.read_csv("add _gateways_to_diagnostics.csv")
 
-for orgId,name,orgType,parId,intId in zip(data["id"],data["name"],data["type"],data["partnerId"],data["integratorId"]) :
+for gateId,siteID in zip(gatedata[0],gatedata[1]) :
+     cursor.execute('''INSERT INTO organisations ("serialNumber", "siteID") VALUES (%s, %s)''',(gateId, siteID))
+     connection.commit()
+
+gatedeldata = pd.read_csv("remove _gateways_from_diagnostics.csv")
+
+for gateId in gatedeldata[0] :
+     cursor.execute('''DELETE FROM organisations WHERE serialNumber=%s ''',(gateId))
+     connection.commit()
+
+data = pd.read_csv("add _organisation_to_diagnostics.csv")
+
+for orgId,name,orgType,parId,intId in zip(data[0],data[1],data[2],data[3],data[4]) :
      cursor.execute('''INSERT INTO organisations ("id", "name", "type", "partnerId", "integratorId") VALUES (%s, %s, %s, %s, %s)''',(orgId, name, orgType, parId, intId))
      connection.commit()
-    
-data = pd.read_csv("sites.csv")
 
-for siteId,siteName,siteLocation,orgId in zip(data["id"],data["name"],data["location"],data["orgId"]) :
+data = pd.read_csv("remove _organisation_from_diagnostics.csv")
+
+for orgId in data[0] :
+     cursor.execute('''DELETE FROM organisations WHERE id=%s ''',(orgId))
+     connection.commit()
+
+
+    
+siteAdd = pd.read_csv("add _sites_to_diagnostics.csv")
+
+for siteId,siteName,siteLocation,orgId in zip(siteAdd[0],siteAdd[1],siteAdd[2],siteAdd[3]) :
     cursor.execute('''INSERT INTO sites ("id", "name", "location", "organisationId") VALUES (%s, %s, %s, %s)''',(siteId, siteName, siteLocation, orgId))
     connection.commit()
-    
-data = pd.read_csv("accesspoints.csv")
 
-for accessPointId,accessPointName,orgId,siteId,accessPointType in zip(data["id"],data["name"],data["orgId"],data["siteId"],data["accessPointType"]) :
-     cursor.execute('''INSERT INTO access_points ("id", "name", "forAccess", "forAttendance", "siteId") VALUES (%s, %s, %s, %s, %s)''',(accessPointId, accessPointName, orgId, siteId, accessPointType))
+sitedel = pd.read_csv("remove _organisation_from_diagnostics.csv")
+
+for siteId in sitedel[0] :
+     cursor.execute('''DELETE FROM sites WHERE id=%s ''',(siteId))
      connection.commit()
     
-data = pd.read_csv("devices.csv")
+data = pd.read_csv("add _access_points_to_diagnostics.csv")
+
+for accessPointId,accessPointName,orgId,siteId,accessPointType in zip(data[0],data[1]) :
+     cursor.execute('''INSERT INTO access_points ("id", "name") VALUES (%s, %s)''',(accessPointId, accessPointName))
+     connection.commit()
+
+accpdel = pd.read_csv("remove _access_points_from_diagnostics.csv")
+
+for accpId in accpdel[0] :
+     cursor.execute('''DELETE FROM access_points WHERE id=%s ''',(accpId))
+     connection.commit()
+
+
+    
+data = pd.read_csv("add _devices_to_diagnostics.csv")
 
 for serialNumber,accessPointId,deviceType in zip(data["serialNumber"],data["accessPointId"],data["deviceType"]) :
-     cursor.execute('''INSERT INTO devices ("serialNumber", "accessPointId", "deviceType") VALUES (%s, %s, %s)''',(serialNumber, accessPointId, deviceType))
+     cursor.execute('''INSERT INTO devices ("serialNumber", "accessPointId") VALUES (%s, %s)''',(serialNumber, accessPointId))
+     connection.commit()
+
+devdel = pd.read_csv("remove _devices_from_diagnostics.csv")
+
+for devId in devdel[0] :
+     cursor.execute('''DELETE FROM devices WHERE serialNumber=%s ''',(devId))
      connection.commit()
     
 
